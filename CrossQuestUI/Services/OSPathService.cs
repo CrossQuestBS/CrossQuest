a0df0dc9539a4301afa89082dcdb87e9542acd6b
Lillie <75392499+iLillie@users.noreply.github.com>
2026-02-07 19:10:16 +0100

    feat: update QuestService paths to be cross-platform

--- CrossQuestUI/Services/OSPathService.cs
+++ CrossQuestUI/Services/OSPathService.cs
@@ -32,6 +32,22 @@ namespace CrossQuestUI.Services
             { UnknownPlatform, ""}
         };
         
+        private static Dictionary<OSPlatform, string> AdbPaths = new ()
+        {
+            { OSPlatform.Windows, @"/SDK/platform-tools/adb.exe" },
+            { OSPlatform.Linux, @"/SDK/platform-tools/adb" },
+            { OSPlatform.OSX, @"/SDK/platform-tools/adb" },
+            { UnknownPlatform, ""}
+        };
+        
+        private static Dictionary<OSPlatform, string> ApkSignerPaths = new ()
+        {
+            { OSPlatform.Windows, @"/SDK/build-tools/34.0.0/apksigner.exe" },
+            { OSPlatform.Linux, @"/SDK/build-tools/34.0.0/apksigner" },
+            { OSPlatform.OSX, @"/SDK/build-tools/34.0.0/apksigner" },
+            { UnknownPlatform, ""}
+        };
+        
         public static OSPlatform CurrentPlatform = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
             ? OSPlatform.Windows
             : (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
@@ -50,6 +66,7 @@ namespace CrossQuestUI.Services
             return Path.Join(prefixPath, AndroidPlayerPaths[CurrentPlatform]);
         }
 
-
+        public static string AdBPath(string androidPlayer) => Path.Join(androidPlayer, AdbPaths[CurrentPlatform]);
+        public static string ApkSignerPath(string androidPlayer) => Path.Join(androidPlayer, ApkSignerPaths[CurrentPlatform]);
     }
 }
