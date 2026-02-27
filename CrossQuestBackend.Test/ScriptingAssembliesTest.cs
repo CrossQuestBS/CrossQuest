627dede2a06b405fa672b5694c936383c3f58066
Lillie <75392499+iLillie@users.noreply.github.com>
2026-02-27 10:49:11 +0100

    Add CrossQuestBackend with Test

---
+++ CrossQuestBackend.Test/ScriptingAssembliesTest.cs
@@ -0,0 +1,23 @@
+﻿using CrossQuestBackend.Compilation;
+
+namespace CrossQuestBackend.Test;
+
+public class Tests
+{
+    private ScriptingAssemblies _scriptingAssemblies;
+    private readonly string _expectedValue = "{\"names\":[\"Unity.Assembly.dll\",\"Unity.Assembly2.dll\",\"User.Assembly.dll\",\"User.Assembly2.dll\"],\"types\":[2,2,16,16]}";
+    
+    [SetUp]
+    public void Setup()
+    {
+        List<string> unityAssemblies = ["Unity.Assembly.dll", "Unity.Assembly2.dll"];
+        List<string> userAssemblies = ["User.Assembly.dll", "User.Assembly2.dll"];
+        _scriptingAssemblies = new ScriptingAssemblies(unityAssemblies, userAssemblies);
+    }
+
+    [Test]
+    public void ShouldConvertToJson()
+    {
+        Assert.That(_scriptingAssemblies.ToJson(), Is.EqualTo(_expectedValue));
+    }
+}
---
+++ CrossQuestBackend/Compilation/ScriptingAssemblies.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace CrossQuestBackend.Compilation;
+
+public class ScriptingAssemblies
+{
+
+   private const int UnityAssemblyType = 2;
+   private const int UserAssemblyType = 16;
+   
+   public ScriptingAssemblies(List<string> unityAssemblies, List<string> userAssemblies)
+   {
+      names = [];
+      types = [];
+      
+      foreach (var assembly in unityAssemblies)
+      {
+         names.Add(assembly);
+         types.Add(UnityAssemblyType);
+      }
+      
+      foreach (var assembly in userAssemblies)
+      {
+         names.Add(assembly);
+         types.Add(UserAssemblyType);
+      }
+   }
+
+   public List<string> names;
+   public List<int> types;
+
+   public string ToJson()
+   {
+      return JsonConvert.SerializeObject(this);
+   }
+}
