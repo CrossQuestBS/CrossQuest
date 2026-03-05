6f4d4508956aaf80eb383bd6417b500bb598af48
Lillie <75392499+iLillie@users.noreply.github.com>
2026-03-05 21:47:14 +0100

    Take into another PR

--- CrossQuestBackend/Unity/Resource/ScriptingAssemblies.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using Newtonsoft.Json;
-
-namespace CrossQuestBackend.Unity.Resource;
-
-internal record ScriptingAssemblies(
-    [JsonProperty("names")] List<string> Names,
-    [JsonProperty("types")] List<int> Types
-);
