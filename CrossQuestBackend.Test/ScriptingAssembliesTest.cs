b8c6b11378fd57455bb63a5128f70ef8154e696a
Lillie <75392499+iLillie@users.noreply.github.com>
2026-03-05 21:34:29 +0100

    refactor ScriptingAssemblies

--- CrossQuestBackend.Test/ScriptingAssembliesTest.cs
+++ CrossQuestBackend.Test/ScriptingAssembliesTest.cs
@@ -1,35 +1,30 @@
 ﻿using CrossQuestBackend.Unity.Compilation;
+using CrossQuestBackend.Unity.Models;
 
 namespace CrossQuestBackend.Test;
 
 public class ScriptingAssembliesTests
 {
     private ScriptingAssemblies _scriptingAssemblies;
-    private const string ExpectedValue = "{\"names\":[\"Unity.Assembly.dll\",\"Unity.Assembly2.dll\",\"User.Assembly.dll\",\"User.Assembly2.dll\"],\"types\":[2,2,16,16]}";
 
     [SetUp]
     public void Setup()
     {
         List<string> unityAssemblies = ["Unity.Assembly.dll", "Unity.Assembly2.dll"];
         List<string> userAssemblies = ["User.Assembly.dll", "User.Assembly2.dll"];
-        _scriptingAssemblies = new ScriptingAssemblies(unityAssemblies, userAssemblies);
+        _scriptingAssemblies = UnityResourceService.GenerateScriptingAssemblies(unityAssemblies, userAssemblies);
     }
 
     [Test]
     public void ShouldProperlyInsertTypes()
     {
-        Assert.That(_scriptingAssemblies.types, Is.EqualTo([2, 2, 16, 16]));
-    }
-    
-    [Test]
-    public void ShouldProperlyInsertNames()
-    {
-        Assert.That(_scriptingAssemblies.names, Is.EqualTo(["Unity.Assembly.dll", "Unity.Assembly2.dll", "User.Assembly.dll", "User.Assembly2.dll"]));
+        Assert.That(_scriptingAssemblies.Types, Is.EqualTo([2, 2, 16, 16]));
     }
 
     [Test]
-    public void ShouldConvertToJson()
+    public void ShouldProperlyInsertNames()
     {
-        Assert.That(_scriptingAssemblies.ToJson(), Is.EqualTo(ExpectedValue));
+        Assert.That(_scriptingAssemblies.Names,
+            Is.EqualTo(["Unity.Assembly.dll", "Unity.Assembly2.dll", "User.Assembly.dll", "User.Assembly2.dll"]));
     }
 }
