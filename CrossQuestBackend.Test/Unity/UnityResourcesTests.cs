604e81a8c7d7e9d385a5b9949aabe007fbdaa899
Lillie <75392499+iLillie@users.noreply.github.com>
2026-03-09 15:02:51 +0100

    Update tests

--- CrossQuestBackend.Test/Unity/UnityResourcesTests.cs
+++ CrossQuestBackend.Test/Unity/UnityResourcesTests.cs
@@ -5,27 +5,37 @@ namespace CrossQuestBackend.Test.Unity;
 
 public class UnityResourcesTests
 {
-
         private ScriptingAssemblies _scriptingAssemblies;
 
         [SetUp]
         public void Setup()
         {
-            List<string> unityAssemblies = ["Unity.Assembly.dll", "Unity.Assembly2.dll"];
-            List<string> userAssemblies = ["User.Assembly.dll", "User.Assembly2.dll"];
+            List<string> unityAssemblies = ["Path/To/Place/Unity.Assembly.dll", "Path/To/Place/Unity.Assembly2.dll"];
+            List<string> userAssemblies = ["Path/To/Place/User.Assembly.dll", "Path/To/Place/User.Assembly2.dll"];
             _scriptingAssemblies = UnityResources.ScriptingAssemblies(unityAssemblies, userAssemblies);
         }
-
-        [Test]
-        public void ShouldProperlyInsertTypes()
+        
+        public class ScriptingAssembliesTests : UnityResourcesTests
         {
-            Assert.That(_scriptingAssemblies.Types, Is.EqualTo([2, 2, 16, 16]));
-        }
+            [Test]
+            public void ShouldProperlyInsertTypes()
+            {
+                Assert.That(_scriptingAssemblies.Types, 
+                    Is.EqualTo([2, 2, 16, 16]));
+            }
 
-        [Test]
-        public void ShouldProperlyInsertNames()
-        {
-            Assert.That(_scriptingAssemblies.Names,
-                Is.EqualTo(["Unity.Assembly.dll", "Unity.Assembly2.dll", "User.Assembly.dll", "User.Assembly2.dll"]));
+            [Test]
+            public void ShouldProperlyInsertNames()
+            {
+                Assert.That(_scriptingAssemblies.Names,
+                    Is.EqualTo(["Unity.Assembly.dll", "Unity.Assembly2.dll", "User.Assembly.dll", "User.Assembly2.dll"]));
+            }
+        
+            [Test]
+            public void ShouldReturnValidJSON()
+            {
+                Assert.That(_scriptingAssemblies.AsJson(),
+                    Is.EqualTo("{\"Names\":[\"Unity.Assembly.dll\",\"Unity.Assembly2.dll\",\"User.Assembly.dll\",\"User.Assembly2.dll\"],\"Types\":[2,2,16,16]}"));
+            }
         }
 }
